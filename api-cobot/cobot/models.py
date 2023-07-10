7b2799f67b68063942531610d5c17346857a7fc7
imfabra <daniel_fabra@outlook.com>
2023-07-10 00:40:14 -0500

    Crear modelos para el almacenamiento de movimientos

--- api-cobot/cobot/models.py
+++ api-cobot/cobot/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    name = models.CharField(max_length=30)
+    motor1_angle = models.IntegerField(default=0)
+    motor2_angle = models.IntegerField(default=0)
+    motor3_angle = models.IntegerField(default=0)
+    motor4_angle = models.IntegerField(default=0)
+    motor5_angle = models.IntegerField(default=0)
+
+
+class Movement(models.Model):
+    name = models.CharField(max_length=30)
+    points = models.ManyToManyField(Point, blank=True)
+    gripper = models.BooleanField(default=False)
+
+
+class Sequence(models.Model):
+    name = models.CharField(max_length=30)
+    movements = models.ManyToManyField(Movement, null=False)
