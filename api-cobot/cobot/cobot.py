5a526e8710608dfa278bb77cb58394bfd01e0e4c
dhenaom <dhenaom51@gmail.com>
2023-08-12 02:56:47 +0100

    Modificar tasks.py para aplicar algoritmo de control

--- api-cobot/cobot/cobot.py
+++ api-cobot/cobot/cobot.py
@@ -1,9 +1,9 @@
-from cobot.tasks import *
+from cobot.tasks import CobotTasks
 import json
 
 class Cobot:
     def __init__(self):
-        self.angulo_anterior = cobot_angles.delay().get()
+        self.angulo_anterior = CobotTasks.cobot_angles.delay().get()
         self.angulo_actuales = None
 
     def _speed_angles(self, current_angles, expected_angles, velocity):
@@ -34,26 +34,26 @@ class Cobot:
                 self.angulo_actuales = data[1]
                 velocidad = self._speed_angles(self.angulo_anterior, self.angulo_actuales, 40)
 
-                cobot_points.delay(command, type, self.angulo_actuales, velocidad)
+                CobotTasks.cobot_points.delay(command, type, self.angulo_actuales, velocidad)
 
                 self.angulo_anterior = self.angulo_actuales
 
             elif type == "movement":
-                cobot_movements.delay(command, type, data)
+                CobotTasks.cobot_movements.delay(command, type, data)
 
             elif type == "sequence":
-                cobot_sequences.delay(command, type, data)
+                CobotTasks.cobot_sequences.delay(command, type, data)
         
         elif command == "cli":
             if type=="home":
                 self.angulo_anterior = [0.0]*5
-                cobot_home_reset.delay()
+                CobotTasks.cobot_home_reset.delay()
             elif type == "angles":
-                return cobot_angles.delay().get()
+                return CobotTasks.cobot_angles.delay().get()
             elif type == "motors_off":
                 print("En cobot task")
-                motors_off.delay()
+                CobotTasks.motors_off.delay()
             elif type == "motors_on":
-                x = motors_on.delay().get()
+                x = CobotTasks.motors_on.delay().get()
                 print("En cobot data: ", x)
                 return x
--- api-cobot/cobot/tasks.py
+++ api-cobot/cobot/tasks.py
@@ -4,86 +4,93 @@ from .modules.bluetooth.Bluetooth import BT
 from time import sleep
 import json
 
-motors=RMDX()
-motors.setup()
-motors.getMotorList()
-bt=BT()
-
-@app.task
-def cobot_home_reset():
-    motors.going_to_zero()
-    sleep(5)
-
-@app.task
-def cobot_points(command, type, data, velocity):
-    print("--------------- Point ------------------")
-    # Aqui ejecutar movimiento robot (Retardar movimiento) -> data[1]
-    motors.send_motion(data, velocity)
-    # ----------------------------------------------------------
-    print("Command: " + str(command))
-    print("Type: " + str(type))
-    print("Data: " + str(data[1]))
-    print("----------------------------------------")
-
-@app.task
-def cobot_movements(command, type, data, velocity):
-    print("-------------- Movement ----------------")
-    print("Command: " + str(command))
-    print("Type: " + str(type))
-    print("Data: " + str(data))
-    print("----------------------------------------")
-    print("Name: " + str(data[0]))
-    print("Gripper: " + str(data[1]))
-    print("----- Si lista vacia - True Gripper ----")
-    status_list = False
-    for i in data[2]:
-        if len(i) > 0:
-            status_list = True
-            # Aqui ejecutar movimiento robot (Retardar movimiento) -> i[1]
-            motors.send_motion(i[1],[40,40,40,40,40])
-            sleep(2)
-            print(i)
-    if status_list == False:
-        # Aqui llamar metodo gripper-> comando: data[1]
-        if(data[1] == True):
-            bt.run("C")
-        elif(data[1] == False):
-            bt.run("A")
-        print("Ejecutar Gripper A: ", data[1])
-    print("----------------------------------------")
-    return 
 
 
-@app.task
-def cobot_sequences(command, type, data):
-    pointsList = []
-    print("-------------- Sequences ---------------")
-    print("Command: " + str(command))
-    print("Type: " + str(type))
-    print("Data: " + str(data))
-    print("----------------------------------------")
-    angMaxA=0
-    for i in data[1]:
-        if len(i) != 0:
-            # print(i)
-            cobot_movements(command, type, i)
-            # print("anguloMaxA: ",angMaxA)
-    print(pointsList)
+class CobotTasks:
+    motors=RMDX()
+    motors.setup()
+    motors.getMotorList()
+    bt=BT()
 
+    @staticmethod
+    @app.task
+    def cobot_home_reset():
+        CobotTasks.motors.going_to_zero()
+        sleep(5)
+    
+    @staticmethod
+    @app.task
+    def cobot_points(command, type, data, velocity):
+        print("--------------- Point ------------------")
+        # Aqui ejecutar movimiento robot (Retardar movimiento) -> data[1]
+        CobotTasks.motors.send_motion(data, velocity)
+        # ----------------------------------------------------------
+        print("Command: " + str(command))
+        print("Type: " + str(type))
+        print("Data: " + str(data[1]))
+        print("----------------------------------------")
 
-@app.task
-def cobot_angles():
-    anglesX = json.dumps(motors.get_angle_value(1))
-    return anglesX
+    @staticmethod
+    @app.task
+    def cobot_movements(command, type, data, velocity):
+        print("-------------- Movement ----------------")
+        print("Command: " + str(command))
+        print("Type: " + str(type))
+        print("Data: " + str(data))
+        print("----------------------------------------")
+        print("Name: " + str(data[0]))
+        print("Gripper: " + str(data[1]))
+        print("----- Si lista vacia - True Gripper ----")
+        status_list = False
+        for i in data[2]:
+            if len(i) > 0:
+                status_list = True
+                # Aqui ejecutar movimiento robot (Retardar movimiento) -> i[1]
+                CobotTasks.motors.send_motion(i[1],[40,40,40,40,40])
+                sleep(2)
+                print(i)
+        if status_list == False:
+            # Aqui llamar metodo gripper-> comando: data[1]
+            if(data[1] == True):
+                CobotTasks.bt.run("C")
+            elif(data[1] == False):
+                CobotTasks.bt.run("A")
+            print("Ejecutar Gripper A: ", data[1])
+        print("----------------------------------------")
+        return 
+    
+    @staticmethod
+    @app.task
+    def cobot_sequences(command, type, data):
+        pointsList = []
+        print("-------------- Sequences ---------------")
+        print("Command: " + str(command))
+        print("Type: " + str(type))
+        print("Data: " + str(data))
+        print("----------------------------------------")
+        angMaxA=0
+        for i in data[1]:
+            if len(i) != 0:
+                # print(i)
+                CobotTasks.cobot_movements(command, type, i)
+                # print("anguloMaxA: ",angMaxA)
+        print(pointsList)
 
-@app.task
-def motors_off():
-    print("Entre a tasks ejecutar")
-    motors.motors_off()
+    @staticmethod
+    @app.task
+    def cobot_angles():
+        anglesX = json.dumps(CobotTasks.motors.get_angle_value(1))
+        return anglesX
+    
+    @staticmethod
+    @app.task
+    def motors_off():
+        print("Entre a tasks ejecutar")
+        CobotTasks.motors.motors_off()
 
-@app.task
-def motors_on():
-    motors.motors_on()
-    anglesOn = json.dumps(motors.get_angle_value(1))
-    return anglesOn
-
+    @staticmethod
+    @app.task
+    def motors_on():
+        CobotTasks.motors.motors_on()
+        anglesOn = json.dumps(CobotTasks.motors.get_angle_value(1))
+        return anglesOn
