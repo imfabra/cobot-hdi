892460a43c6cc5f87542d1d68d9845fbf1e5798d
dhenaom <dhenaom51@gmail.com>
2023-08-12 20:09:39 +0100

    Agregar puntos BT y hacer clase en tasks.py

--- api-cobot/cobot/cobot.py
+++ api-cobot/cobot/cobot.py
@@ -1,59 +1,23 @@
-from cobot.tasks import CobotTasks
+from cobot.tasks import cli_tasks, executor_tasks
 import json
 
 class Cobot:
     def __init__(self):
-        self.angulo_anterior = CobotTasks.cobot_angles.delay().get()
-        self.angulo_actuales = None
-
-    def _speed_angles(self, current_angles, expected_angles, velocity):
-        if isinstance(velocity, int):
-            velocity = [velocity]*len(current_angles)
-
-        full_angles = [abs((a)-(d)) for a, d in zip(current_angles, expected_angles)]
-
-        max_angle = max(full_angles)
-        speeds = [round((v*angle/max_angle), 2) for angle, v in zip(full_angles, velocity)]
-
-        print(f'''
-            \r------- Velocidad Motores ------------
-            \rAngulos actuales:  {current_angles},
-            \rAngulos deseados:  {expected_angles},
-            \rRecorrido motores: {full_angles},
-            \rVelocidad:         {speeds}
-            \r--------------------------------------
-        ''')
-
-        return speeds
+        pass
 
     # command: play
     # type: point or movement or sequence
     def run_command(self, command, type, data=""):
         if command == "play":
-            if type == "point":
-                self.angulo_actuales = data[1]
-                velocidad = self._speed_angles(self.angulo_anterior, self.angulo_actuales, 40)
-
-                CobotTasks.cobot_points.delay(command, type, self.angulo_actuales, velocidad)
-
-                self.angulo_anterior = self.angulo_actuales
-
-            elif type == "movement":
-                CobotTasks.cobot_movements.delay(command, type, data)
-
-            elif type == "sequence":
-                CobotTasks.cobot_sequences.delay(command, type, data)
+            executor_tasks.delay(command, type, data)
         
         elif command == "cli":
             if type=="home":
-                self.angulo_anterior = [0.0]*5
-                CobotTasks.cobot_home_reset.delay()
+                cli_tasks.delay("home")
             elif type == "angles":
-                return CobotTasks.cobot_angles.delay().get()
+                return cli_tasks.delay("angles").get()
             elif type == "motors_off":
-                print("En cobot task")
-                CobotTasks.motors_off.delay()
+                cli_tasks.delay("motors_off")
             elif type == "motors_on":
-                x = CobotTasks.motors_on.delay().get()
-                print("En cobot data: ", x)
+                x = cli_tasks.delay("motors_on").get()
                 return x
--- api-cobot/cobot/tasks.py
+++ api-cobot/cobot/tasks.py
@@ -7,32 +7,55 @@ import json
 
 
 class CobotTasks:
-    motors=RMDX()
-    motors.setup()
-    motors.getMotorList()
-    bt=BT()
-
-    @staticmethod
-    @app.task
-    def cobot_home_reset():
-        CobotTasks.motors.going_to_zero()
-        sleep(5)
     
-    @staticmethod
-    @app.task
-    def cobot_points(command, type, data, velocity):
+    def __init__(self):
+        self.motors=RMDX()
+        self.motors.setup()
+        self.motors.getMotorList()
+        self.bt=BT()
+
+        self.current_angles = [0.0]*5
+        self.expected_angles = [0.0]*5
+
+    def _speed_angles(self, current_angles, expected_angles, velocity=10):
+        if isinstance(velocity, int):
+            velocity = [velocity]*len(current_angles)
+
+        full_angles = [abs((a)-(d)) for a, d in zip(current_angles, expected_angles)]
+
+        max_angle = max(full_angles)
+        speeds = [5 if round((v * angle / max_angle), 2) < 5 else round((v * angle / max_angle), 2) for angle, v in zip(full_angles, velocity)]
+
+        print(f'''
+            \r------- Velocidad Motores ------------
+            \rAngulos actuales:  {current_angles},
+            \rAngulos deseados:  {expected_angles},
+            \rRecorrido motores: {full_angles},
+            \rVelocidad:         {speeds}
+            \r--------------------------------------
+        ''')
+
+        return speeds
+
+    def cobot_home_reset(self):
+        self.current_angles = [0.0]*5
+        self.motors.going_to_zero()
+        sleep(3)
+    
+    def cobot_points(self, command, type, data):
         print("--------------- Point ------------------")
         # Aqui ejecutar movimiento robot (Retardar movimiento) -> data[1]
-        CobotTasks.motors.send_motion(data, velocity)
+        self.expected_angles = data[1]
+        velocity = self._speed_angles(self.current_angles, self.expected_angles, 50)
+        self.motors.send_motion(self.expected_angles, velocity)
+        self.current_angles = self.expected_angles
         # ----------------------------------------------------------
         print("Command: " + str(command))
         print("Type: " + str(type))
         print("Data: " + str(data[1]))
         print("----------------------------------------")
 
-    @staticmethod
-    @app.task
-    def cobot_movements(command, type, data, velocity):
+    def cobot_movements(self, command, type, data):
         print("-------------- Movement ----------------")
         print("Command: " + str(command))
         print("Type: " + str(type))
@@ -42,26 +65,25 @@ class CobotTasks:
         print("Gripper: " + str(data[1]))
         print("----- Si lista vacia - True Gripper ----")
         status_list = False
-        for i in data[2]:
-            if len(i) > 0:
+        for angles in data[2]:
+            if len(angles) > 0:
                 status_list = True
                 # Aqui ejecutar movimiento robot (Retardar movimiento) -> i[1]
-                CobotTasks.motors.send_motion(i[1],[40,40,40,40,40])
-                sleep(2)
-                print(i)
+                # self.motors.send_motion(i[1],[40,40,40,40,40])
+                self.cobot_points(command, type, angles)
+                print(angles)
+                sleep(3)
         if status_list == False:
             # Aqui llamar metodo gripper-> comando: data[1]
             if(data[1] == True):
-                CobotTasks.bt.run("C")
+                self.bt.run("C")
             elif(data[1] == False):
-                CobotTasks.bt.run("A")
+                self.bt.run("A")
             print("Ejecutar Gripper A: ", data[1])
         print("----------------------------------------")
         return 
     
-    @staticmethod
-    @app.task
-    def cobot_sequences(command, type, data):
+    def cobot_sequences(self, command, type, data):
         pointsList = []
         print("-------------- Sequences ---------------")
         print("Command: " + str(command))
@@ -72,25 +94,41 @@ class CobotTasks:
         for i in data[1]:
             if len(i) != 0:
                 # print(i)
-                CobotTasks.cobot_movements(command, type, i)
+                self.cobot_movements(command, type, i)
                 # print("anguloMaxA: ",angMaxA)
         print(pointsList)
 
-    @staticmethod
-    @app.task
-    def cobot_angles():
-        anglesX = json.dumps(CobotTasks.motors.get_angle_value(1))
+    def cobot_angles(self):
+        anglesX = json.dumps(self.motors.get_angle_value(1))
         return anglesX
     
-    @staticmethod
-    @app.task
-    def motors_off():
+    def motors_off(self):
         print("Entre a tasks ejecutar")
-        CobotTasks.motors.motors_off()
+        self.motors.motors_off()
 
-    @staticmethod
-    @app.task
-    def motors_on():
-        CobotTasks.motors.motors_on()
-        anglesOn = json.dumps(CobotTasks.motors.get_angle_value(1))
+    def motors_on(self):
+        self.motors.motors_on()
+        anglesOn = self.motors.get_angle_value(1)
         return anglesOn
+
+cobottasks = CobotTasks()
+
+@app.task
+def cli_tasks(cli):
+    if cli == "home":
+        cobottasks.cobot_home_reset()
+    elif cli == "angles":
+        cobottasks.cobot_angles()
+    elif cli == "motors_off":
+        cobottasks.motors_off()
+    elif cli == "motors_on":
+        return json.dumps(cobottasks.motors_on())
+
+@app.task
+def executor_tasks(command, type, data):
+    if type == "point":
+        cobottasks.cobot_points(command, type, data)
+    elif type == "movement":
+        cobottasks.cobot_movements(command, type, data)
+    elif type == "sequence":
+        cobottasks.cobot_movements(command, type, data)
